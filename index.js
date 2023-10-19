08c8c96718cd143909fdfe8df1e4856decd7d76a
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-10-19 13:39:58 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -11,7 +11,31 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+// console.log(process.env.DB_USER, process.env.DB_PASSWORD);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.vnja0wm.mongodb.net/?retryWrites=true&w=majority`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // db ping
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+    // console.log("db not connecting");
+  }
+}
+run().catch(console.dir);
 app.listen(port, (req, res) => {
   console.log(`Listening at port ${port}`);
 });
